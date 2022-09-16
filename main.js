b74ff6fde9c64233a75437d915aeb0be65d3d30b
Yuulis <79734873+Yuulis@users.noreply.github.com>
2022-09-16 14:22:06 +0900

    フォーム送信後のメッセージを修正

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const form = FormApp.getActiveForm();
 const spreadSheet = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty("sheet_id"));
 const reservationApplication = spreadSheet.getSheetByName("予約申請");
 const reservationStatus = spreadSheet.getSheetByName("予約状況");
@@ -39,33 +38,21 @@ function receivedApplication(e) {
   const date_index = results[1];
   const room_index = results[2];
 
+  // ハッシュ値作成
+  const time = date + " "  + start_time + "~" + end_time;
+  const hash = dataToHash(time + " " + room + " " + group_name);
+
   // 予約可能なら
   if (check){
-    // ハッシュ値作成
-    const time = date + " "  + start_time + "~" + end_time;
-    const hash = dataToHash(time + " " + room + " " + group_name);
-
     // 予約申請シートに予約内容を書き込む
     addReservationApplication(hash, group_name, room, time);
 
     // 予約状況シートの更新
     updateReserationStatus(group_name, start_time, end_time, date_index, room_index);
   }
-}
 
-function addReservationApplication(hash, group_name, room, time) {
-  reservationApplication.appendRow([hash, room, group_name, time]);
-}
-
-function updateReserationStatus(group_name, start_time, end_time, date_index, room_index) {
-  // 更新前のデータ取得
-  const pre_value = reservationStatus.getRange(date_index, room_index).getValue();
-
-  // 更新データ作成
-  const time = start_time + "~" + end_time;
-  const value = (pre_value === "") ? group_name + " " + time : pre_value + "\n" + group_name + " " + time;
-
-  reservationStatus.getRange(date_index, room_index).setValue(value);
+  // フォーム送信後のメッセージ編集
+  updateConfirmationMessage(check, hash, group_name, room, time);
 }
 
 
@@ -87,11 +74,10 @@ function checkAvailability(room, date, start_time, end_time) {
             let pre_start_time = value.slice(-11, -6);
             let pre_end_time = value.slice(-5);
 
-            if (start_time < pre_end_time && pre_start_time < end_time) {
-              return [false, null, null];
-            }
-            else {
+            if (pre_end_time <= start_time || end_time <= pre_start_time) {
               return [true, i + 3, j + 2 + 1];
+            } else {
+              return [false, null, null];
             }
           }
         }
@@ -103,9 +89,40 @@ function checkAvailability(room, date, start_time, end_time) {
   return [false, null, null];
 }
 
+
 function getDataOfReservationStatus(start_row, start_col) {
   const last_row = reservationStatus.getLastRow() - start_row + 1;
   const last_col = reservationStatus.getMaxColumns() - start_col + 1;
   
   return reservationStatus.getRange(start_row, start_col, last_row, last_col).getValues();
+}
+
+
+function addReservationApplication(hash, group_name, room, time) {
+  reservationApplication.appendRow([hash, room, group_name, time]);
+}
+
+
+function updateReserationStatus(group_name, start_time, end_time, date_index, room_index) {
+  // 更新前のデータ取得
+  const pre_value = reservationStatus.getRange(date_index, room_index).getValue();
+
+  // 更新データ作成
+  const time = start_time + "~" + end_time;
+  const value = (pre_value === "") ? group_name + " " + time : pre_value + "\n" + group_name + " " + time;
+
+  reservationStatus.getRange(date_index, room_index).setValue(value);
+}
+
+
+function updateConfirmationMessage(check, hash, group_name, room, time) {
+  let message = "";
+  if (check) {
+    message = `予約が完了しました。\n予約コードは以下の通りです。予約変更の際に必要となりますので、この画面をスクリーンショットしておくことをお勧めします。\n \n【予約コード】\n${hash}\n\n【予約内容】\n使用団体 : ${group_name}\n使用教室 : ${room}\n使用日時 : ${time}\n`;
+  } else {
+    message = `予約を受け付けできませんでした。\n条件を変えて再度予約してください。\n`;
+  }
+
+  const form = FormApp.getActiveForm();
+  form.setConfirmationMessage(message);
 }
